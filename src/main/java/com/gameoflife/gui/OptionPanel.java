4ea0514c35a1de76e8368ca1dc4d0bf9c5c35817
h1r0kuu <abhazim.nurbat@gmail.com>
2023-03-18 22:26:54 +0100

    Add start, stop, clear, randomize btn and text label for probability

--- src/main/java/com/gameoflife/gui/OptionPanel.java
+++ src/main/java/com/gameoflife/gui/OptionPanel.java
@@ -2,20 +2,54 @@ package com.gameoflife.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class OptionPanel extends JPanel {
-    public static final int OptionPanelWidth = (int)(GameFrame.WIDTH * 0.3);
-    public static final int OptionPanelHeight = GameFrame.HEIGHT;
-    public OptionPanel() {
-        Button button = new Button();
-        setBackground(Color.WHITE);
-        setBounds(GamePanel.GamePanelWidth, 0, OptionPanelWidth, OptionPanelHeight);
-        setLayout(null);
+    public OptionPanel(GamePanel gamePanel) {
+        Button startButton = new Button();
+        startButton.setLabel("Start");
+        startButton.addActionListener(e -> gamePanel.setPaused(true));
 
-        button.setBackground(Color.RED);
-        button.setLabel("Stop");
-        button.setBounds(0, 0, 111, 111);
-        button.setLocation(0, 0);
-        add(button);
+        Button stopButton = new Button();
+        stopButton.setLabel("Stop");
+        stopButton.addActionListener(e -> gamePanel.setPaused(false));
+
+        Button clearButton = new Button();
+        clearButton.addActionListener(e -> gamePanel.clearGrid());
+        clearButton.setLabel("Clear");
+
+        TextField textField =new TextField();
+
+
+
+        Button randomizeButton = new Button();
+        randomizeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int probability = Integer.parseInt(textField.getText());
+                    if(probability < 0 || probability > 100) {
+                        JOptionPane.showMessageDialog(gamePanel, "Probability must be between 0 and 100");
+                    } else {
+                        gamePanel.randomizeGrid(probability);
+                    }
+                }
+                catch (NumberFormatException error) {
+                    JOptionPane.showMessageDialog(gamePanel, error.getMessage());
+                }
+            }
+        });
+
+        randomizeButton.setLabel("Randomize");
+
+        setBackground(Color.WHITE);
+
+        add(startButton);
+        add(stopButton);
+        add(clearButton);
+        add(randomizeButton);
+        add(textField);
     }
 }
