94a1424e4f88b6082c821198cfa1ef2416a6d20e
h1r0kuu <abhazim.nurbat@gmail.com>
2023-04-08 22:21:11 +0200

    OptionState refactoring, separate some piece of code, theme setup

--- src/main/java/com/h1r0kuu/gameoflife/OptionController.java
+++ src/main/java/com/h1r0kuu/gameoflife/OptionController.java
@@ -2,12 +2,17 @@ package com.h1r0kuu.gameoflife;
 
 import com.h1r0kuu.gameoflife.entity.Cell;
 import com.h1r0kuu.gameoflife.entity.Grid;
+import com.h1r0kuu.gameoflife.theme.Theme;
+import com.h1r0kuu.gameoflife.theme.ThemeManager;
 import javafx.animation.AnimationTimer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.geometry.Point2D;
+import javafx.geometry.Bounds;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
@@ -15,106 +20,142 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
-import javafx.scene.transform.NonInvertibleTransformException;
 import javafx.scene.transform.Scale;
 
 public class OptionController {
     private Grid grid;
-
     private final int FPS_SET = 180;
     private int gameSpeedNum = 10;
-    private double scale = 1.0;
-
-    @FXML private AnchorPane anchorPane;
-    @FXML private Canvas canvas;
-    @FXML private Button playToggle;
-    @FXML private ImageView playImage;
-    @FXML private Slider gameSpeed;
-    @FXML private Slider gameZoom;
-    @FXML private Button clearButton;
-    @FXML private Button nextGenerationButton;
-    @FXML private Button drawState;
-    @FXML private Button moveState;
-    @FXML private Button selectState;
-    @FXML private Label generationCounter;
-
     private boolean isPaused = false;
+    private boolean pausedByButton = false;
     private AnimationTimer animationTimer;
-
     private OptionState optionState = OptionState.DRAWING;
-
     private double mouseX, mouseY;
-
     private int generation = 0;
-    private double canvasTranslateX = 0;
-    private double canvasTranslateY = 0;
-    private double canvasScale = 1;
+    private double canvasX, canvasY;
+    private Scale scaleTransform;
+    private double zoom = 1.0;
+    private double startX, startY, endX, endY;
+    private static final Color DEFAULT_BG_COLOR = Color.web("#151310");
+    private static final ThemeManager themeManager = new ThemeManager();
+    public static Theme getCurrentTheme() { return themeManager.getCurrentTheme(); }
+
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private Canvas canvas;
+    @FXML
+    private Button playToggle;
+    @FXML
+    private ImageView playImage;
+    @FXML
+    private Slider gameSpeed;
+    @FXML
+    private Slider gameZoom;
+    @FXML
+    private Button clearButton;
+    @FXML
+    private Button nextGenerationButton;
+    @FXML
+    private Button drawState;
+    @FXML
+    private Button moveState;
+    @FXML
+    private Button selectState;
+    @FXML
+    private Button showBorderButton;
+    @FXML
+    private Label generationCounter;
+    @FXML
+    private Label gameSpeedLabel;
+
+    @FXML
+    private ChoiceBox<String> themes;
+
     @FXML
     public void initialize() {
         setupGrid();
-        GraphicsContext graphics = canvas.getGraphicsContext2D();
-        Scale zoom = new Scale(1, 1);
+        setupThemes();
 
+        anchorPane.setStyle("-fx-background-color: " + DEFAULT_BG_COLOR);
+        scaleTransform = new Scale(1, 1);
+        canvas.getTransforms().add(scaleTransform);
         canvas.setOnScroll(event -> {
-//            double mouseX = event.getX();
-//            double mouseY = event.getY();
-//
-//            double currScaleX = canvas.getScaleX();
-//            double currScaleY = canvas.getScaleY();
-//
-//            double newScaleX = Math.max(currScaleX * zoomFactor, 0.1);
-//            double newScaleY = Math.max(currScaleY * zoomFactor, 0.1);
-//
-//            canvas.setTranslateX(canvas.getTranslateX() + mouseX);
-//            canvas.setTranslateY(canvas.getTranslateY() + mouseY);
-//
-//            canvas.setScaleX(newScaleX);
-//            canvas.setScaleY(newScaleY);
-//
-//            canvas.setTranslateX(canvas.getTranslateX() - mouseX);
-//            canvas.setTranslateY(canvas.getTranslateY() - mouseY);
-//
-//            event.consume();
-            double zoomFactor = event.getDeltaY() > 0 ? 1.1 : 0.9;
-            canvasScale *= zoomFactor;
+            double zoomFactor = 1.5;
+            if (event.getDeltaY() <= 0) {
+                zoomFactor = 1 / zoomFactor;
+            }
+            double oldScale = canvas.getScaleX();
+            double scale = oldScale * zoomFactor;
+            double f = (scale / oldScale) - 1;
 
-            double oldMouseX = canvasTranslateX + event.getX();
-            double oldMouseY = canvasTranslateY + event.getY();
+            Bounds bounds = canvas.localToScene(canvas.getBoundsInLocal());
+            double dx = (event.getX() - (bounds.getWidth() / 2 + bounds.getMinX()));
+            double dy = (event.getY() - (bounds.getHeight() / 2 + bounds.getMinY()));
+            canvas.setTranslateX(canvas.getTranslateX() - f * dx);
+            canvas.setTranslateY(canvas.getTranslateY() - f * dy);
+            canvas.setScaleX(scale);
+            canvas.setScaleY(scale);
+        });
+        setupCanvasMouseHandlers();
 
-            canvas.setTranslateX(canvasTranslateX);
-            canvas.setTranslateY(canvasTranslateY);
-            canvas.setScaleX(canvasScale);
-            canvas.setScaleY(canvasScale);
-
-            double newMouseX = canvasTranslateX + event.getX();
-            double newMouseY = canvasTranslateY + event.getY();
-
-            canvasTranslateX += oldMouseX - newMouseX;
-            canvasTranslateY += oldMouseY - newMouseY;
-
-            event.consume();
+        drawState.setOnMouseClicked(e -> {
+            setOptionState(OptionState.DRAWING);
+        });
+        moveState.setOnMouseClicked(e -> {
+            setOptionState(OptionState.MOVING);
+        });
+        selectState.setOnMouseClicked(e -> {
+            setOptionState(OptionState.SELECTING);
         });
 
+        startGameLoop();
+    }
+
+    private void setupThemes() {
+        ObservableList<String> themeNames = FXCollections.observableArrayList(themeManager.getThemes().stream().map(t -> t.THEME_NAME).toList());
+        themes.setItems(themeNames);
+        if(themeNames.size() > 0) themes.setValue(themeNames.get(0));
+
+        themes.getSelectionModel().selectedIndexProperty().addListener((observableValue, number, number2) -> {
+            String newTheme = themes.getItems().get((Integer) number2);
+            themeManager.changeTheme(newTheme);
+        });
+    }
+
+    private void setupCanvasMouseHandlers() {
         canvas.setOnMouseMoved(e -> {
             int gridx = (int) (e.getX() / (Cell.CELL_SIZE * 1.0));
             int gridy = (int) (e.getY() / (Cell.CELL_SIZE * 1.0));
             grid.hoverGrid(gridx, gridy);
         });
+        canvas.setOnMousePressed(event -> {
+            mouseX = event.getSceneX();
+            mouseY = event.getSceneY();
 
-        canvas.setOnMouseClicked(e -> {
+            canvasX = canvas.getLayoutX();
+            canvasY = canvas.getLayoutY();
+
+            startX = event.getX();
+            startY = event.getY();
+
+            endX = event.getX();
+            endY = event.getY();
+
             if(optionState.equals(OptionState.DRAWING)) {
-                int gridx = (int) (e.getX() / (Cell.CELL_SIZE * 1.0));
-                int gridy = (int) (e.getY() / (Cell.CELL_SIZE * 1.0));
-                if(e.isPrimaryButtonDown()) {
-                    grid.getCell(gridx, gridy).setAlive(true);
-                    fillCell(graphics, Grid.BORDER_COLOR, Cell.ALIVE_CELL_COLOR, gridx, gridy);
+                setPaused(true);
+                int gridx = (int) (startX / (Cell.CELL_SIZE * 1.0));
+                int gridy = (int) (startY / (Cell.CELL_SIZE * 1.0));
+
+                if(event.isPrimaryButtonDown()) {
+                    grid.reviveCell(gridx, gridy);
                 } else {
-                    grid.getCell(gridx, gridy).setAlive(false);
-                    fillCell(graphics, Grid.BORDER_COLOR, Cell.DEAD_CELL_COLOR, gridx, gridy);
+                    grid.killCell(gridx, gridy);
                 }
             } else if(optionState.equals(OptionState.MOVING)) {
-                mouseX = e.getX();
-                mouseY = e.getY();
+
+                mouseX = event.getX();
+                mouseY = event.getY();
             }
         });
 
@@ -122,44 +163,54 @@ public class OptionController {
             if(optionState.equals(OptionState.DRAWING)) {
                 int gridx = (int) (e.getX() / (Cell.CELL_SIZE * 1.0));
                 int gridy = (int) (e.getY() / (Cell.CELL_SIZE * 1.0));
-                grid.hoverGrid(gridx, gridy);
 
+                grid.hoverGrid(gridx, gridy);
                 if(e.isPrimaryButtonDown()) {
-                    grid.getCell(gridx, gridy).setAlive(true);
-                    fillCell(graphics, Grid.BORDER_COLOR, Cell.ALIVE_CELL_COLOR, gridx, gridy);
+                    grid.reviveCell(gridx, gridy);
                 } else {
-                    grid.getCell(gridx, gridy).setAlive(false);
-                    fillCell(graphics, Grid.BORDER_COLOR, Cell.DEAD_CELL_COLOR, gridx, gridy);
+                    grid.killCell(gridx, gridy);
                 }
             } else if(optionState.equals(OptionState.MOVING)) {
-                double deltaX = e.getX() - mouseX;
-                double deltaY = e.getY() - mouseY;
-                canvas.setTranslateX(canvas.getTranslateX() + deltaX);
-                canvas.setTranslateY(canvas.getTranslateY() + deltaY);
-                mouseX = e.getX();
-                mouseY = e.getY();
+                double deltaX = e.getSceneX() - mouseX;
+                double deltaY = e.getSceneY() - mouseY;
+                canvas.setLayoutX(canvasX + deltaX);
+                canvas.setLayoutY(canvasY + deltaY);
+
+            } else if(optionState.equals(OptionState.SELECTING)) {
+                endX = e.getX();
+                endY = e.getY();
+                grid.selectRange(startX, startY, endX, endY);
             }
         });
 
-        drawState.setOnMouseClicked(e -> {
-            moveState.setStyle(null);
-            selectState.setStyle(null);
-            optionState = OptionState.DRAWING;
-            drawState.setStyle("-fx-background-color: #68ff00");
+        canvas.setOnMouseReleased(e -> {
+            if(optionState.equals(OptionState.DRAWING)) {
+                if(!pausedByButton) setPaused(false);
+            }
         });
-        moveState.setOnMouseClicked(e -> {
-            selectState.setStyle(null);
-            drawState.setStyle(null);
-            optionState = OptionState.MOVING;
-            moveState.setStyle("-fx-background-color: #68ff00");
-        });
-        selectState.setOnMouseClicked(e -> {
-            moveState.setStyle(null);
-            drawState.setStyle(null);
-            optionState = OptionState.SELECTING;
-            selectState.setStyle("-fx-background-color: #68ff00");
-        });
+    }
 
+    private void setOptionState(OptionState optionState) {
+        this.optionState = optionState;
+
+        drawState.setStyle("");
+        moveState.setStyle("");
+        selectState.setStyle("");
+
+        switch (optionState) {
+            case DRAWING:
+                drawState.setStyle("-fx-background-color: #68ff00; -fx-text-fill: white");
+                break;
+            case MOVING:
+                moveState.setStyle("-fx-background-color: #68ff00; -fx-text-fill: white");
+                break;
+            case SELECTING:
+                selectState.setStyle("-fx-background-color: #68ff00; -fx-text-fill: white");
+                break;
+        }
+    }
+
+    public void startGameLoop() {
         animationTimer = new AnimationTimer() {
             final double timePerFrame = 1_000_000_000.0 / FPS_SET;
             long lastFrame = System.nanoTime();
@@ -171,7 +222,7 @@ public class OptionController {
 
             @Override
             public void handle(long l) {
-                long updateInterval = (long) (1000.0 / (gameSpeedNum / 10) );
+                long updateInterval = (long) (1000.0 / gameSpeedNum );
 
                 now = System.nanoTime();
                 if(now - lastFrame >= timePerFrame) {
@@ -211,22 +262,19 @@ public class OptionController {
         canvas.setScaleY(1.0);
     }
 
-    public static void fillCell(GraphicsContext graphics, Color borderColor, Color cellColor, int gridx, int gridy) {
-        graphics.setFill(borderColor);
-        graphics.fillRect(gridx * Cell.CELL_SIZE, gridy * Cell.CELL_SIZE, Cell.CELL_SIZE, Cell.CELL_SIZE);
-        graphics.setFill(cellColor);
-        graphics.fillRect((gridx * Cell.CELL_SIZE) + 1, (gridy * Cell.CELL_SIZE) + 1, Cell.CELL_SIZE - 2, Cell.CELL_SIZE - 2);
+    public void setPaused(boolean pause) {
+        isPaused = pause;
+        if(pause) {
+            playImage.setImage(new Image(getClass().getResource("icons/pause.png").toExternalForm()));
+        } else {
+            animationTimer.start();
+            playImage.setImage(new Image(getClass().getResource("icons/play.png").toExternalForm()));
+        }
     }
 
     public void togglePlay(MouseEvent mouseEvent) {
-        if(isPaused) {
-            isPaused = false;
-            animationTimer.start();
-            playImage.setImage(new Image(getClass().getResource("icons/play.png").toExternalForm()));
-        } else {
-            isPaused = true;
-            playImage.setImage(new Image(getClass().getResource("icons/pause.png").toExternalForm()));
-        }
+        setPaused(!isPaused);
+        pausedByButton = isPaused;
     }
 
     public void nextGen() {
@@ -246,11 +294,22 @@ public class OptionController {
 
     public void changeGameSpeed(MouseEvent mouseEvent) {
         gameSpeedNum = (int)gameSpeed.getValue();
+        gameSpeedLabel.setText(String.format("%d gen/s", gameSpeedNum));
     }
+
 
     public void changeGameScale(MouseEvent mouseEvent) {
         double zoom = gameZoom.getValue() / 100;
         canvas.setScaleX(zoom);
         canvas.setScaleY(zoom);
+    }
+
+    public void toggleGrid(MouseEvent mouseEvent) {
+        grid.setShowBorders(!grid.isShowBorders());
+        if(grid.isShowBorders()) {
+            showBorderButton.setStyle("-fx-background-color: #68ff00");
+        } else {
+            showBorderButton.setStyle(null);
+        }
     }
 }
