2a7e3beb27883f6ad7eef201ed3828abf1ad0bad
h1r0kuu <abhazim.nurbat@gmail.com>
2023-04-15 21:15:29 +0200

    Cell info text, getText for multiple values

--- src/main/java/com/h1r0kuu/gameoflife/utils/LabelUtility.java
+++ src/main/java/com/h1r0kuu/gameoflife/utils/LabelUtility.java
@@ -4,6 +4,7 @@ public class LabelUtility {
     public static final String FPS = "%d FPS";
     public static final String GENERATION_COUNTER = "Generation: %d";
     public static final String GAME_SPEED = "%d gps";
+    public static final String CELL_INFO = "%d,%d=%s";
 
     public static String getText(String value, String format) {
         return String.format(format, value);
@@ -11,5 +12,9 @@ public class LabelUtility {
 
     public static String getText(int value, String format) {
         return String.format(format, value);
+    }
+
+    public static String getText(String format, Object... values) {
+        return String.format(format, values);
     }
 }
